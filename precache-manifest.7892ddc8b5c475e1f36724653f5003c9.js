6d42fbec4aef74d6ae98c10cdfb832c1bf707243
Bjornson <arthurbearnyson@mail.ru>
2020-08-30 17:21:16 +0600

    Updates

---
+++ precache-manifest.7892ddc8b5c475e1f36724653f5003c9.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "facb72dbf90b4c3dc92559512749516a",
+    "url": "/prana-cell/index.html"
+  },
+  {
+    "revision": "12a1f6f697eb23cdb09e",
+    "url": "/prana-cell/static/css/main.6f020c33.chunk.css"
+  },
+  {
+    "revision": "9931829bad557e77eb76",
+    "url": "/prana-cell/static/js/2.16e774c7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/prana-cell/static/js/2.16e774c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12a1f6f697eb23cdb09e",
+    "url": "/prana-cell/static/js/main.903fd42b.chunk.js"
+  },
+  {
+    "revision": "fdfffd50cf2c5e647e0f",
+    "url": "/prana-cell/static/js/runtime-main.ac994fdb.js"
+  },
+  {
+    "revision": "eeadf3222bbae73a113921de9f1c6f7a",
+    "url": "/prana-cell/static/media/Montserrat-Bold.eeadf322.woff2"
+  },
+  {
+    "revision": "c7b975f3e8445b8b780e6e2ddc8fdaaa",
+    "url": "/prana-cell/static/media/Montserrat-Regular.c7b975f3.woff2"
+  },
+  {
+    "revision": "a0d372074a83c2090d898c344ca2483d",
+    "url": "/prana-cell/static/media/Montserrat-SemiBold.a0d37207.woff2"
+  },
+  {
+    "revision": "ef9b431e9b21c0791e85565847358665",
+    "url": "/prana-cell/static/media/background.ef9b431e.jpg"
+  },
+  {
+    "revision": "01235db691ecd218a9da4cb99a14bf1f",
+    "url": "/prana-cell/static/media/down-arrow.01235db6.svg"
+  },
+  {
+    "revision": "0c2c8e68fc40be403a13dc8a914d1f6d",
+    "url": "/prana-cell/static/media/gulmira.0c2c8e68.png"
+  },
+  {
+    "revision": "e47e742429b8dd2cafcd71027a1f4478",
+    "url": "/prana-cell/static/media/home.e47e7424.webp"
+  },
+  {
+    "revision": "e0b467a19920b542ce9c21223abe5f3a",
+    "url": "/prana-cell/static/media/laboratory.e0b467a1.jpg"
+  },
+  {
+    "revision": "3c9c4af4ac4ff10526bf073844fe52dc",
+    "url": "/prana-cell/static/media/logo (2).3c9c4af4.jpg"
+  },
+  {
+    "revision": "e6956db22f6b55500046d41bb128484a",
+    "url": "/prana-cell/static/media/mail.e6956db2.svg"
+  },
+  {
+    "revision": "e35dcc8c9fed1be724c05d478cc9f1ca",
+    "url": "/prana-cell/static/media/schemeImmuno.e35dcc8c.jpg"
+  },
+  {
+    "revision": "6b50648a7945c757fbb903965e038099",
+    "url": "/prana-cell/static/media/treat.6b50648a.jpg"
+  }
+]);
